0994bb9cb1ac9518eac60e2ff934bda906d76b63
Lucas Sequeira <lucassequeiradaniel@gmail.com>
2020-08-23 00:10:59 -0300

    Creacion de variables de entorno

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
+require('dotenv').config();
+
 const app = require('./server/config');
 
 function main() {
     try {
-        app.listen(3000);
-        console.log('Servidor UP en puerto 3000');
+        app.listen(process.env.PORT);
+        console.log('Servidor UP en puerto',process.env.PORT);
     } catch (error) {
         console.log('Ocurrio un error',error);
     }
